5a9e26771d7bf0c120cbc1a32d11d665d46ea7df
Alexandr Romanov <aromanov07@gmail.com>
2022-06-26 21:30:01 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OversizeComponents
+
+A description of this package.
---
+++ Tests/OversizeComponentsTests/OversizeComponentsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import OversizeComponents
+
+final class OversizeComponentsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(OversizeComponents().text, "Hello, World!")
+    }
+}
