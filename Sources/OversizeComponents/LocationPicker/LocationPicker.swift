317083d30c0e6506e2fb895faf2bd7dda0f9b676
Aleksandr Romanov <admin@Aleksandrs-MBP.localdomain>
2024-12-29 23:12:40 +0300

    Changes

--- Sources/OversizeComponents/LocationPicker/LocationPicker.swift
+++ Sources/OversizeComponents/LocationPicker/LocationPicker.swift
@@ -9,137 +9,137 @@ import OversizeUI
 import SwiftUI
 
 #if os(iOS)
-public struct LocationPicker: View {
-    @Environment(\.theme) private var theme: ThemeSettings
+    public struct LocationPicker: View {
+        @Environment(\.theme) private var theme: ThemeSettings
 
-    @Binding private var coordinates: CLLocationCoordinate2D
-    @Binding private var positionName: String?
-    private let label: String
+        @Binding private var coordinates: CLLocationCoordinate2D
+        @Binding private var positionName: String?
+        private let label: String
 
-    private let saveButtonText: String?
-    @State private var offset = CGPoint(x: 0, y: 0)
-    @State private var showModal = false
-    @State private var isSelected = false
+        private let saveButtonText: String?
+        @State private var offset = CGPoint(x: 0, y: 0)
+        @State private var showModal = false
+        @State private var isSelected = false
 
-    public init(label: String, coordinates: Binding<CLLocationCoordinate2D>, positionName: Binding<String?>, saveButtonText: String? = nil) {
-        self.label = label
-        self.saveButtonText = saveButtonText
-        _coordinates = coordinates
-        _positionName = positionName
-    }
-
-    public var body: some View {
-        Button {
-            showModal.toggle()
-        } label: {
-            HStack(spacing: .xxSmall) {
-                Text(label)
-                    .bold()
-                    .headline()
-                    .foregroundColor(.onSurfacePrimary)
-            }
-            Spacer()
-
-            if let positionName {
-                Text(positionName)
-                    .subheadline()
-                    .foregroundColor(.onPrimarySecondary)
-            }
-
-            IconDeprecated(.chevronDown, color: .onSurfacePrimary)
+        public init(label: String, coordinates: Binding<CLLocationCoordinate2D>, positionName: Binding<String?>, saveButtonText: String? = nil) {
+            self.label = label
+            self.saveButtonText = saveButtonText
+            _coordinates = coordinates
+            _positionName = positionName
         }
-        .frame(minWidth: 0, maxWidth: .infinity)
-        .padding()
-        .background(
-            RoundedRectangle(
-                cornerRadius: Radius.medium,
-                style: .continuous
-            )
-            .fill(Color.surfaceSecondary)
-            .overlay(
+
+        public var body: some View {
+            Button {
+                showModal.toggle()
+            } label: {
+                HStack(spacing: .xxSmall) {
+                    Text(label)
+                        .bold()
+                        .headline()
+                        .foregroundColor(.onSurfacePrimary)
+                }
+                Spacer()
+
+                if let positionName {
+                    Text(positionName)
+                        .subheadline()
+                        .foregroundColor(.onPrimarySecondary)
+                }
+
+                IconDeprecated(.chevronDown, color: .onSurfacePrimary)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .background(
                 RoundedRectangle(
                     cornerRadius: Radius.medium,
                     style: .continuous
                 )
-                .stroke(
-                    theme.borderTextFields
-                        ? Color.border
-                        : Color.surfaceSecondary,
-                    lineWidth: CGFloat(theme.borderSize)
+                .fill(Color.surfaceSecondary)
+                .overlay(
+                    RoundedRectangle(
+                        cornerRadius: Radius.medium,
+                        style: .continuous
+                    )
+                    .stroke(
+                        theme.borderTextFields
+                            ? Color.border
+                            : Color.surfaceSecondary,
+                        lineWidth: CGFloat(theme.borderSize)
+                    )
                 )
             )
-        )
-        .sheet(isPresented: $showModal) {
-            modal
+            .sheet(isPresented: $showModal) {
+                modal
+            }
+            .onChange(of: coordinates) {
+                updateCityName(coordinate: $0)
+            }
         }
-        .onChange(of: coordinates) {
-            updateCityName(coordinate: $0)
-        }
-    }
 
-    public var modal: some View {
-        ZStack {
-            MapViewDepecated(centerCoordinate: $coordinates)
-                .ignoresSafeArea()
+        public var modal: some View {
+            ZStack {
+                MapViewDepecated(centerCoordinate: $coordinates)
+                    .ignoresSafeArea()
 
-            VStack {
-                Spacer()
+                VStack {
+                    Spacer()
 
-                MaterialSurface {
-                    VStack(alignment: .center, spacing: .xxSmall) {
-                        if let positionName {
-                            Text(positionName)
-                                .title3()
-                                .foregroundColor(.onSurfacePrimary)
+                    MaterialSurface {
+                        VStack(alignment: .center, spacing: .xxSmall) {
+                            if let positionName {
+                                Text(positionName)
+                                    .title3()
+                                    .foregroundColor(.onSurfacePrimary)
+                            }
+                            Text("\(coordinates.latitude), \(coordinates.longitude)")
+                                .subheadline()
+                                .foregroundColor(.onSurfaceSecondary)
                         }
-                        Text("\(coordinates.latitude), \(coordinates.longitude)")
-                            .subheadline()
-                            .foregroundColor(.onSurfaceSecondary)
                     }
-                }
-                .surfaceContentMargins(.small)
-                .controlRadius(.large)
-                .multilineTextAlignment(.center)
+                    .surfaceContentMargins(.small)
+                    .controlRadius(.large)
+                    .multilineTextAlignment(.center)
 
-            }.padding()
+                }.padding()
+            }
+            .navigationBar(label, style: .fixed($offset)) {
+                BarButton(.close)
+            } trailingBar: {
+                BarButton(.secondary(saveButtonText ?? "Save", action: {
+                    isSelected = true
+                    showModal.toggle()
+                }))
+            }
         }
-        .navigationBar(label, style: .fixed($offset)) {
-            BarButton(.close)
-        } trailingBar: {
-            BarButton(.secondary(saveButtonText ?? "Save", action: {
-                isSelected = true
-                showModal.toggle()
-            }))
-        }
-    }
 
-    private func updateCityName(coordinate: CLLocationCoordinate2D) {
-        let loc = CLLocation(
-            latitude: coordinate.latitude,
-            longitude: coordinate.longitude
-        )
+        private func updateCityName(coordinate: CLLocationCoordinate2D) {
+            let loc = CLLocation(
+                latitude: coordinate.latitude,
+                longitude: coordinate.longitude
+            )
 
-        let geocoder: CLGeocoder = .init()
+            let geocoder: CLGeocoder = .init()
 
-        geocoder.reverseGeocodeLocation(loc) { placemarks, error in
-            if error != nil { return }
-            if let firstLocation = placemarks?.first as? CLPlacemark {
-                if let locality = firstLocation.locality {
-                    positionName = locality
+            geocoder.reverseGeocodeLocation(loc) { placemarks, error in
+                if error != nil { return }
+                if let firstLocation = placemarks?.first as? CLPlacemark {
+                    if let locality = firstLocation.locality {
+                        positionName = locality
 
-                } else if let subLocality = firstLocation.subLocality {
-                    positionName = subLocality
-                } else {
-                    positionName = firstLocation.name
+                    } else if let subLocality = firstLocation.subLocality {
+                        positionName = subLocality
+                    } else {
+                        positionName = firstLocation.name
+                    }
                 }
             }
         }
     }
-}
 
-extension CLLocationCoordinate2D: @retroactive Equatable {}
+    extension CLLocationCoordinate2D: @retroactive Equatable {}
 
-public func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
-    lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
-}
+    public func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
 #endif
--- Sources/OversizePhotoComponents/AvatarPicker/AvatarPickerView.swift
+++ Sources/OversizePhotoComponents/AvatarPicker/AvatarPickerView.swift
@@ -6,54 +6,54 @@
 import OversizeUI
 import SwiftUI
 #if canImport(UIKit)
-import UIKit
+    import UIKit
 #endif
 
 #if os(iOS)
-public struct AvatarPickerView: View {
-    @Binding private var avatar: UIImage?
-    @State var isShowPicker: Bool = false
+    public struct AvatarPickerView: View {
+        @Binding private var avatar: UIImage?
+        @State var isShowPicker: Bool = false
 
-    public init(avatar: Binding<UIImage?>) {
-        _avatar = avatar
-    }
-
-    public var body: some View {
-        Button {
-            isShowPicker.toggle()
-        } label: {
-            avatarLabel
+        public init(avatar: Binding<UIImage?>) {
+            _avatar = avatar
         }
-        .buttonStyle(.scale)
-        .sheet(isPresented: $isShowPicker) {
-            GellaryPhotoPickerView(selection: $avatar)
+
+        public var body: some View {
+            Button {
+                isShowPicker.toggle()
+            } label: {
+                avatarLabel
+            }
+            .buttonStyle(.scale)
+            .sheet(isPresented: $isShowPicker) {
+                GellaryPhotoPickerView(selection: $avatar)
+            }
         }
-    }
 
-    @ViewBuilder
-    private var avatarLabel: some View {
-        if let avatar {
-            Avatar(avatar: Image(uiImage: avatar))
-                .controlSize(.large)
-        } else {
-            ZStack {
-                Circle()
-                    .fill(Color.surfaceSecondary)
-                    .frame(width: Space.xxxLarge.rawValue, height: Space.xxxLarge.rawValue)
+        @ViewBuilder
+        private var avatarLabel: some View {
+            if let avatar {
+                Avatar(avatar: Image(uiImage: avatar))
+                    .controlSize(.large)
+            } else {
+                ZStack {
+                    Circle()
+                        .fill(Color.surfaceSecondary)
+                        .frame(width: Space.xxxLarge.rawValue, height: Space.xxxLarge.rawValue)
 
-                Image.Base.Camera.fill
-                    .renderingMode(.template)
-                    .resizable()
-                    .foregroundColor(Color.onSurfaceSecondary)
-                    .frame(width: 48, height: 48)
+                    Image.Base.Camera.fill
+                        .renderingMode(.template)
+                        .resizable()
+                        .foregroundColor(Color.onSurfaceSecondary)
+                        .frame(width: 48, height: 48)
+                }
             }
         }
     }
-}
 
-// struct AvatarPickerView_Previews: PreviewProvider {
+    // struct AvatarPickerView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        AvatarPickerView()
 //    }
-// }
+    // }
 #endif
